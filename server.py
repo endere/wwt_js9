219fee80485767c40508cc212d45985f2ba7f525
Erik <erik.end@gmail.com>
2017-09-20 20:47:14 -0700

    deployment

---
+++ server.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+from flask import Flask, request, send_file
+import base64
+app = Flask(__name__)
+
+
+@app.route('/', methods=['POST', 'GET'])
+def image_storage():
+    if request.method == 'POST':
+        print(len('data:image/png;base64,'))
+        data = base64.b64decode(request.data[22:])
+        app.stored_image = open("saved.png", "wb")
+        app.stored_image.write(data)
+        app.stored_image.close()
+        return 'received'
+    elif request.method == 'GET':
+        return send_file('saved.png', mimetype='image/png')
+if __name__ == '__main__':
+    app.run()
