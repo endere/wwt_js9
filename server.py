f93b5874f1a091dcfbfcb19ccfca4d7079da5856
Erik <erik.end@gmail.com>
2017-09-21 10:34:59 -0700

    testing returns on server for debugging

--- server.py
+++ server.py
@@ -4,27 +4,16 @@ import random
 import base64
 app = Flask(__name__)
 
-
-
-def uniqueid():
-    seed = random.getrandbits(32)
-    while True:
-        yield seed
-        seed += 1
-app.num = uniqueid()
-
-@app.route('/<num>', methods=['GET'])
-def image_return(num):
-    return send_file('saved.png', mimetype='image/png')
-
-
-@app.route('/', methods=['POST'])
+@app.route('/', methods=['POST', 'GET'])
 def image_storage():
-    data = base64.b64decode(request.data[22:])
-    app.stored_image = open("saved.png", "wb")
-    app.stored_image.write(data)
-    app.stored_image.close()
-    return send_file('saved.png', mimetype='image/png')
+    if request.method == 'POST':
+        data = base64.b64decode(request.data[22:])
+        app.stored_image = open("saved.png", "wb")
+        app.stored_image.write(data)
+        app.stored_image.close()
+        return 'success'
+    else:
+        return send_file('saved.png', mimetype='image/png')
 
 @app.route('/images.wtml', methods=['GET'])
 def wtml_return():
