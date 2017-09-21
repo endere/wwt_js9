ddb8c503b6a0817c92f8a9f40cd871ac51cb5101
Erik <erik.end@gmail.com>
2017-09-21 09:00:23 -0700

    testing returns on server for debugging

--- server.py
+++ server.py
@@ -1,18 +1,30 @@
 #!/usr/bin/python
 from flask import Flask, request, send_file
+import random
 import base64
 app = Flask(__name__)
 
 
-@app.route('/<num>', methods=['POST', 'GET'])
-def image_storage(num):
-    if request.method == 'POST':
-        data = base64.b64decode(request.data[22:])
-        app.stored_image = open("saved.png", "wb")
-        app.stored_image.write(data)
-        app.stored_image.close()
-        return num
-    elif request.method == 'GET':
-        return send_file('saved.png', mimetype='image/png')
+@app.route('/<num>', methods=['GET'])
+def image_return():
+    return send_file('saved.png', mimetype='image/png')
+
+
+@app.route('/', methods=['POST'])
+def image_storage():
+    data = base64.b64decode(request.data[22:])
+    app.stored_image = open("saved.png", "wb")
+    app.stored_image.write(data)
+    app.stored_image.close()
+    return next(app.num)
+
+def uniqueid():
+    seed = random.getrandbits(32)
+    while True:
+        yield seed
+        seed += 1
+
+
 if __name__ == '__main__':
     app.run()
+    app.num = uniqueid()
