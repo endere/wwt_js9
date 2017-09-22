9fcdf84f6ee3075c545e1124d25d74ab83de8598
Erik <erik.end@gmail.com>
2017-09-22 09:14:44 -0700

    adding coordinates

--- server.py
+++ server.py
@@ -6,12 +6,12 @@ app = Flask(__name__)
 @app.route('/', methods=['POST', 'GET'])
 def image_storage():
     if request.method == 'POST':
-        data = base64.b64decode(request.data['url'][22:])
-        app.stored_image = open("saved.png", "wb")
-        app.stored_image.write(data)
-        app.stored_image.close()
+        # data = base64.b64decode(request.data[22:])
+        # app.stored_image = open("saved.png", "wb")
+        # app.stored_image.write(data)
+        # app.stored_image.close()
+        print(request.data)
         print(request.data['Dec'])
-        print(request.data['Ra'])
         return 'success'
     else:
         return send_file('saved.png', mimetype='image/png')
