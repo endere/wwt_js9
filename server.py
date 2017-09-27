b73265e31c89019a7cccfd2f30a8efcb15927444
Erik <erik.end@gmail.com>
2017-09-27 10:11:31 -0700

    adding front end to server

--- server.py
+++ server.py
@@ -25,6 +25,12 @@ def wtml_return():
 def wwt_js9_home():
     return send_file('public/js9-1.12/WWT.html')
 
+
+# @app.route('/wwtcdn', methods=['GET'])
+# def wwtcdn():
+#     return redirect('http://www.worldwidetelescope.org/scripts/wwtsdk.aspx')
+
+
 @app.route('/<file>', methods=['GET'])
 def give_file(file):
     return send_file('public/js9-1.12/{}'.format(file))
