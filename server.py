2a16286b533b06f2c5473af4a339f5e93ed046a6
Erik <erik.end@gmail.com>
2017-09-28 08:25:00 -0700

    working on coordinates again

--- server.py
+++ server.py
@@ -44,12 +44,12 @@ def edit_wtml(dictionary):
         for line in old.readlines():
             try:
                 attribute = list(filter(lambda x: x in line, list(dictionary.keys())))[0]
-                new.write(' ' * 5 + attribute + '=' + '"' + str(dictionary[attribute]) + '"' + '\n')
+                new.write(' ' * 5 + attribute + '=' + '"' + dictionary[attribute].decode('utf-8') + '"' + '\n')
             except IndexError:
                 new.write(line)
 
 
 
 if __name__ == '__main__':
-    app.run()
-
+    # app.run()
+    edit_wtml({'RA': b'1000', 'Dec': b'50'})
