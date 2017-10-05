c3785b69990384d12753621b08f0307e3bfc4817
Erik <erik.end@gmail.com>
2017-10-05 15:23:43 -0700

    more header work

--- extract_metadata.py
+++ extract_metadata.py
@@ -142,10 +142,8 @@ def get_coords_dict(head):
     print(angsep)
     print(pxsep)
     scale = angsep / pxsep
-    print(wcs.wcs_world2pix(p1.ra.value, p1.dec.value, 1))
-    print(wcs.wcs_world2pix([p1.ra.value, p1.dec.value], 1)
-    print(wcs.wcs_world2pix([p2.ra.value, p2.dec.value], 1)
-    print(wcs.all_world2pix(p2.ra.value, p2.dec.value, 1))
+    print(wcs.wcs_world2pix(p1.ra.value, p1.dec.value, 1)[1][0])
+    print(wcs.wcs_world2pix(p2.ra.value, p2.dec.value, 1))
 
     reqd['Rotation'] = -_calculate_rotation_angle('icrs', header)
 
