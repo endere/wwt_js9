067dfc92590110d58e9b9f9d0ba9cf5d6a108f40
Erik <erik.end@gmail.com>
2017-10-12 16:42:37 -0700

    heroku still failing

--- server.py
+++ server.py
@@ -41,6 +41,14 @@ def image_return():
 @app.route('/<address>.png', methods=['GET'])
 def unique_image_return(address):
     try:
+        @after_this_request
+        def remove_file(response):
+            try:
+                os.remove('{}.wtml'.format(address))
+                os.remove('{}.png'.format(address))
+            except:
+                print('file not found')
+            return response
         return send_file('{}.png'.format(address), mimetype='image/png', cache_timeout=1)
     except:
         return send_file('saved.png', mimetype='image/png', cache_timeout=1)
