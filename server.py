1934928e829ebd11a3624eb5104dd724903b2690
Erik <erik.end@gmail.com>
2017-11-07 11:16:46 -0800

    fits verification

--- server.py
+++ server.py
@@ -84,7 +84,7 @@ def give_file(file):
 def verify_fits():
     print('at the fits verification function!!')
     print(json.loads(request.data).decode('utf-8'))
-    print(type(json.loads(request.data).decode('utf-8')))
+    print(type(json.loads(request.data)))
     return 'success'
 
 
